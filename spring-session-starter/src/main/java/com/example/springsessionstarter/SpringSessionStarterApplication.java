76dfa2cfa8a79e7021043ef75e8828e57e04550e
강민규 <gangmingyu@gangmingyuui-MacBookAir.local>
2022-06-05 00:28:46 +0900

    spring session 살펴보기

---
+++ spring-session-starter/src/main/java/com/example/springsessionstarter/SpringSessionStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsessionstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSessionStarterApplication.class, args);
+    }
+
+}
---
+++ spring-session-starter/src/test/java/com/example/springsessionstarter/SpringSessionStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsessionstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
